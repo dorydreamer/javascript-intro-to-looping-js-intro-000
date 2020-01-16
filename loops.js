e60b3d736c847e69c3424c7af9d61ddef9be702a
Doris Le <dorisle@sandiego.edu>
2020-01-16 20:31:39 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,12 +16,12 @@ function whileLoop(n) {
   return `done`
 }
 
-function doWhileLoops(num) {
+function doWhileLoop(num) {
   var i = 0
   function incrementVariable() {
     i = i + 1;
     return i
   }
   do {console.log("I run once regardless.");
-} while (incrementVariable() < 11);
+} while (incrementVariable() < 10);
 }
